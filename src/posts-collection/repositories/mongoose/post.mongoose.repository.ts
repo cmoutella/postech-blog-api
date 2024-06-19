128a6e03ff1159140c37e719026bf9621975f35e
Carolina Moutella <ola@carolinamoutella.com>
2024-06-19 20:21:33 -0300

    Post: config mongo repository

---
+++ src/posts-collection/repositories/mongoose/post.mongoose.repository.ts
@@ -0,0 +1,59 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { PostRepository } from '../post.repository';
+import { InterfacePost } from 'src/posts-collection/schemas/models/post.interface';
+import { Post } from 'src/posts-collection/schemas/post.schema';
+import { Model } from 'mongoose';
+import { DEFAULT_LIMIT } from 'src/shared/default/pagination';
+
+export class PostMongooseRepository implements PostRepository {
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
+  async createPost(newPost: InterfacePost): Promise<void> {
+    const createPost = new this.postModel(newPost);
+
+    await createPost.save();
+  }
+
+  async getAllPosts(page = 1, limit = DEFAULT_LIMIT): Promise<InterfacePost[]> {
+    const offset = (page - 1) * limit;
+
+    return this.postModel.find().skip(offset).limit(limit).exec();
+  }
+
+  async getAllPostsByKeyword(
+    keyword: string,
+    page = 1,
+    limit = DEFAULT_LIMIT,
+  ): Promise<InterfacePost[]> {
+    const offset = (page - 1) * limit;
+
+    // TODO
+    return this.postModel
+      .find((post: InterfacePost) =>
+        post.keyWords.find((word) => word === keyword),
+      )
+      .skip(offset)
+      .limit(limit)
+      .exec();
+  }
+
+  async getOnePost(id: string): Promise<InterfacePost> {
+    return this.postModel.findById(id).exec();
+  }
+
+  async updatePost(id: string, data: Partial<InterfacePost>): Promise<void> {
+    const foundPost = this.postModel.findById(id).exec();
+
+    if (!foundPost) {
+      return null;
+    }
+
+    await this.postModel
+      .updateOne({ _id: id }, { ...foundPost, ...data })
+      .exec();
+  }
+
+  async deletePost(id: string): Promise<void> {
+    await this.postModel.deleteOne({ _id: id }).exec();
+  }
+}
---
+++ src/posts-collection/repositories/post.repository.ts
@@ -0,0 +1,14 @@
+import { InterfacePost } from '../schemas/models/post.interface';
+
+export abstract class PostRepository {
+  abstract getAllPosts(page?: number, limit?: number): Promise<InterfacePost[]>;
+  abstract getAllPostsByKeyword(
+    keyword: string,
+    page?: number,
+    limit?: number,
+  ): Promise<InterfacePost[]>;
+  abstract getOnePost(id: string): Promise<InterfacePost>;
+  abstract createPost(newPost: InterfacePost): Promise<void>;
+  abstract updatePost(id: string, data: Partial<InterfacePost>): Promise<void>;
+  abstract deletePost(id: string): Promise<void>;
+}
