3d7e9f2a3ab9bac359c4731ef9c4e0e99e7fe07a
Carolina Moutella <ola@carolinamoutella.com>
2024-06-26 20:21:28 -0300

    Post: add create post zod validation

--- src/posts-collection/controllers/posts-collection.controller.ts
+++ src/posts-collection/controllers/posts-collection.controller.ts
@@ -8,20 +8,32 @@ import {
   Put,
   UseGuards,
   UseInterceptors,
+  UsePipes,
 } from '@nestjs/common';
 import { InterfacePost } from '../schemas/models/post.interface';
 import { PostsService } from '../services/posts-collection.service';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { LoggingInterceptor } from 'src/shared/interceptors/logging.interceptor';
+import { z } from 'zod';
+import { ZodValidationPipe } from 'src/shared/pipe/zod-validation.pipe';
+
+const createPostSchema = z.object({
+  title: z.string(),
+  text: z.string(),
+  keyWords: z.array(z.string()),
+});
+
+type CreatePost = z.infer<typeof createPostSchema>;
 
 @UseInterceptors(LoggingInterceptor)
 @Controller('post')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UsePipes(new ZodValidationPipe(createPostSchema))
   @Post()
-  async createPost(@Body() post: InterfacePost) {
-    await this.postsService.createPost(post);
+  async createPost(@Body() { title, text, keyWords }: CreatePost) {
+    await this.postsService.createPost({ title, text, keyWords });
   }
 
   @Get()
@@ -29,13 +41,9 @@ export class PostsController {
     return await this.postsService.getAllPosts(page, limit);
   }
 
-  @Get()
-  async getAllPostsByKeyword(
-    @Body() { keyword }: { keyword: string },
-    page?: number,
-    limit?: number,
-  ) {
-    return await this.postsService.getAllPostsByKeyword(keyword, page, limit);
+  @Get('/keyword/:keyword')
+  async getAllPostsByKeyword(@Param('keyword') keyword: string) {
+    return await this.postsService.getAllPostsByKeyword(keyword);
   }
 
   @Get(':id')
