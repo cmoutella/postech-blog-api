2a95c9d9038d91360e103b5a431bd76c96566594
Carolina Moutella <ola@carolinamoutella.com>
2024-07-14 20:48:43 -0300

    Users: hide password from returns and handle errors

--- src/modules/user/repositories/mongoose/user.mongoose.repository.ts
+++ src/modules/user/repositories/mongoose/user.mongoose.repository.ts
@@ -2,18 +2,26 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserRepository } from '../user.repository';
 import { User, UserDocument } from '../../schemas/user.schema';
-import { InterfaceUser } from '../../schemas/models/user.interface';
+import {
+  InterfaceUser,
+  PublicInterfaceUser,
+} from '../../schemas/models/user.interface';
 
 export class UserMongooseRepository implements UserRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async createUser(newUser: InterfaceUser): Promise<void> {
+  async createUser(newUser: InterfaceUser): Promise<PublicInterfaceUser> {
     const createUser = new this.userModel(newUser);
-    await createUser.save();
+    const u = await createUser.save();
+
+    return { id: u._id.toString(), username: u.username };
   }
 
-  async getAllUsers(): Promise<InterfaceUser[]> {
-    return await this.userModel.find().exec();
+  async getAllUsers(): Promise<PublicInterfaceUser[]> {
+    const list = await this.userModel.find().exec();
+    return list.map((u) => {
+      return { username: u.username, id: u._id.toString() };
+    });
   }
 
   async getById(id: string): Promise<InterfaceUser> {
--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -1,10 +1,13 @@
-import { InterfaceUser } from '../schemas/models/user.interface';
+import {
+  InterfaceUser,
+  PublicInterfaceUser,
+} from '../schemas/models/user.interface';
 import { User } from '../schemas/user.schema';
 
 export abstract class UserRepository {
-  abstract createUser(newUser: InterfaceUser): Promise<void>;
-  abstract getAllUsers(): Promise<User[]>;
+  abstract createUser(newUser: InterfaceUser): Promise<PublicInterfaceUser>;
+  abstract getAllUsers(): Promise<PublicInterfaceUser[]>;
   abstract getById(username: string): Promise<User>;
-  abstract getByUsername(username: string): Promise<User>;
+  abstract getByUsername(username: string): Promise<InterfaceUser>;
   abstract deleteUser(id: string): Promise<void>;
 }
