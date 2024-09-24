65208fe4e1e8a065a2532e0b5519359f5a895668
Carolina Moutella <ola@carolinamoutella.com>
2024-09-24 16:14:52 -0300

    fix(PostsModule): fix module dependencies

--- src/modules/posts-collection/posts-collection.module.ts
+++ src/modules/posts-collection/posts-collection.module.ts
@@ -5,15 +5,25 @@ import { PostRepository } from './repositories/post.repository';
 import { PostMongooseRepository } from './repositories/mongoose/post.mongoose.repository';
 import { PostsService } from './services/posts-collection.service';
 import { PostsController } from './controllers/posts-collection.controller';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserMongooseRepository } from '../user/repositories/mongoose/user.mongoose.repository';
+import { User, UserSchema } from '../user/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([
+      { name: Post.name, schema: PostSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   providers: [
     {
       provide: PostRepository,
       useClass: PostMongooseRepository,
+    },
+    {
+      provide: UserRepository,
+      useClass: UserMongooseRepository,
     },
     PostsService,
   ],
